a558bebbf15b323045702f5db6db467e4c8dc920
Alon Belman <alon@rdlogic.com>
2020-02-02 20:16:41 -0800

    test with Timer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -21,6 +22,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  private double m_startTime;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -66,19 +69,35 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
+    m_startTime = Timer.getFPGATimestamp();
+    
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
-  }
 
+
+
+  }
+  
   /**
    * This function is called periodically during autonomous.
    */
   @Override
   public void autonomousPeriodic() {
+    double elapsedTime = m_startTime - Timer.getFPGATimestamp();
+
+    if (elapsedTime % 100 == 0) {
+      System.out.println(elapsedTime);
+    }
+    if (elapsedTime < 3) {
+
+    }
+    else if (elapsedTime < 6) {
+
+    }
   }
 
   @Override
